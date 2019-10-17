c9254e37098da33744658e8d8d16b430f509a2bf
Mike McTighe <m.mctighe10@gmail.com>
2019-10-17 19:53:35 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -5,6 +5,10 @@ has_many :show, through: :characters
   def full_name
     "#{self.first_name} #{self.last_name}"
   end
+  def list_roles
+    characters.collect do |character|
+      "#{character.name} - #{character.show.name}"
+    end
+  end
 
-  
 end
